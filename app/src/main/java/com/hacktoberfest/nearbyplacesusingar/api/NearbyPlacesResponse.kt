bfbb76903ac708057bedd21714671b7c904e95db
dkamalsingh <deepak.kamalsingh@dxc.com>
2021-10-18 17:57:30 +0530

    Implement nearby places API and shows on google map.

---
+++ app/src/main/java/com/hacktoberfest/nearbyplacesusingar/api/NearbyPlacesResponse.kt
@@ -0,0 +1,11 @@
+package com.hacktoberfest.nearbyplacesusingar.api
+
+import com.hacktoberfest.nearbyplacesusingar.model.Place
+import com.google.gson.annotations.SerializedName
+/**
+ * Data class encapsulating a response from the nearby search call to the Places API.
+ */
+data class NearbyPlacesResponse(
+   @SerializedName("results") val results: List<Place>
+)
+
