bc10f209d3380faed4f257532ff77b5274df4c95
dkamalsingh <deepak.kamalsingh@dxc.com>
2021-10-18 18:37:49 +0530

    Implement AR core library for drawing achors on camera preview based on nearby location.

--- app/src/main/java/com/hacktoberfest/nearbyplacesusingar/MainActivity.kt
+++ app/src/main/java/com/hacktoberfest/nearbyplacesusingar/MainActivity.kt
@@ -15,6 +15,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.getSystemService
+import com.appthon.dxc.findnearbyplacesar.ar.PlaceNode
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -22,9 +23,14 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.ar.sceneform.AnchorNode
 import com.hacktoberfest.nearbyplacesusingar.api.NearbyPlacesResponse
 import com.hacktoberfest.nearbyplacesusingar.api.PlacesService
+import com.hacktoberfest.nearbyplacesusingar.ar.PlacesArFragment
 import com.hacktoberfest.nearbyplacesusingar.model.Place
+import com.hacktoberfest.nearbyplacesusingar.model.getPositionVector
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -35,6 +41,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var placesService: PlacesService
+    private lateinit var arFragment: PlacesArFragment
+
+    private var anchorNode: AnchorNode? = null
+    private var markers: MutableList<Marker> = emptyList<Marker>().toMutableList()
 
     // Location
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -56,11 +66,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             return
         }
         setContentView(R.layout.activity_main)
+        arFragment = supportFragmentManager.findFragmentById(R.id.ar_fragment) as PlacesArFragment
         sensorManager = getSystemService()!!
         placesService = PlacesService.create()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        mapFragment =
-            supportFragmentManager.findFragmentById(R.id.maps_fragment) as SupportMapFragment
+        mapFragment = supportFragmentManager.findFragmentById(R.id.maps_fragment) as SupportMapFragment
+        setUpAr()
         setUpMaps()
     }
 
@@ -87,6 +98,15 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager.unregisterListener(this)
     }
 
+    private fun setUpAr() {
+        arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            // Create anchor
+            val anchor = hitResult.createAnchor()
+            anchorNode = AnchorNode(anchor)
+            anchorNode?.setParent(arFragment.arSceneView.scene)
+            addPlaces(anchorNode!!)
+        }
+    }
 
     private fun setUpMaps() {
         mapFragment.getMapAsync { googleMap ->
@@ -136,6 +156,60 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             onSuccess(location)
         }.addOnFailureListener {
             Log.e(TAG, "Could not get location")
+        }
+    }
+
+    private fun showInfoWindow(place: Place) {
+        // Show in AR
+        val matchingPlaceNode = anchorNode?.children?.filter {
+            it is PlaceNode
+        }?.first {
+            val otherPlace = (it as PlaceNode).place ?: return@first false
+            return@first otherPlace == place
+        } as? PlaceNode
+        matchingPlaceNode?.showInfoWindow()
+
+        // Show as marker
+        val matchingMarker = markers.firstOrNull {
+            val placeTag = (it.tag as? Place) ?: return@firstOrNull false
+            return@firstOrNull placeTag == place
+        }
+        matchingMarker?.showInfoWindow()
+    }
+
+    private fun addPlaces(anchorNode: AnchorNode) {
+        val currentLocation = currentLocation
+        if (currentLocation == null) {
+            Log.w(TAG, "Location has not been determined yet")
+            return
+        }
+
+        val places = places
+        if (places == null) {
+            Log.w(TAG, "No places to put")
+            return
+        }
+
+        for (place in places) {
+            // Add the place in AR
+            val placeNode = PlaceNode(this, place)
+            placeNode.setParent(anchorNode)
+            placeNode.localPosition =
+                place.getPositionVector(orientationAngles[0], currentLocation.latLng)
+            placeNode.setOnTapListener { _, _ ->
+                showInfoWindow(place)
+            }
+
+            // Add the place in maps
+            map?.let {
+                val marker = it.addMarker(
+                    MarkerOptions()
+                        .position(place.geometry.location.latLng)
+                        .title(place.name)
+                )
+                marker.tag = place
+                markers.add(marker)
+            }
         }
     }
 
--- app/src/main/java/com/hacktoberfest/nearbyplacesusingar/model/Place.kt
+++ app/src/main/java/com/hacktoberfest/nearbyplacesusingar/model/Place.kt
@@ -1,6 +1,10 @@
 package com.hacktoberfest.nearbyplacesusingar.model
 
 import com.google.android.gms.maps.model.LatLng
+import com.google.ar.sceneform.math.Vector3
+import com.google.maps.android.ktx.utils.sphericalHeading
+import java.lang.Math.cos
+import java.lang.Math.sin
 
 /**
  * A model describing details about a Place (location, name, type, etc.).
@@ -21,6 +25,15 @@ data class Place(
     override fun hashCode(): Int {
         return this.id.hashCode()
     }
+}
+fun Place.getPositionVector(azimuth: Float, latLng: LatLng): Vector3 {
+    val placeLatLng = this.geometry.location.latLng
+    val heading = latLng.sphericalHeading(placeLatLng)
+    val r = -2f
+    val x = r * sin(azimuth + heading).toFloat()
+    val y = 1f
+    val z = r * cos(azimuth + heading).toFloat()
+    return Vector3(x, y, z)
 }
 
 data class Geometry(
